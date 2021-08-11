b3f6f7c94cbcefe8c2b17f83152682d13eaacc90
unknown <breid48@uwo.ca>
2021-08-11 13:03:34 -0700

    Included Support for Config Files

--- pywebshare/api.py
+++ pywebshare/api.py
@@ -1,126 +1,150 @@
 import requests
-import json
+import os
 
-from tenacity import (retry, stop_after_delay,
-                      stop_after_attempt, wait_fixed)
+from tenacity import (retry, stop_after_delay, stop_after_attempt, wait_fixed)
+from configparser import NoOptionError
 
-
+import pywebshare.config as cfg
 
 MAX_DELAY = 10
-MAX_RETRIES = 5
+MAX_RETRIES = 3
 MAX_WAIT = 2
 
 
 class API:
     """
-    Generalized API Wrapper Method Calls with Optional Sub-Arguments
-
+    Generalized requests HTTP Wrapper Method Calls with Optional Sub-Arguments
     """
-    def __init__(self, _api_key=None, portal="main", id=None):
-        self.base_url = "https://proxy.webshare.io/api"
-        self._api_key = _api_key
+
+    def __init__(self, api_key=None, config_path=None, portal="main", user_id=None):
+        self.api_key = api_key
+        self.config_path = config_path
         self.headers = dict()
         self.portal = portal
-        self.id = id # SubUser Portal Use Only
+        self.user_id = user_id  # Sub-user Portal
+        self.base_url = "https://proxy.webshare.io/api"
 
-    @retry(stop=(stop_after_delay(MAX_DELAY) | stop_after_attempt(MAX_RETRIES)), wait=wait_fixed(MAX_WAIT))
+        if not self.api_key:
+            # If an api_key is not explicitly given to the constructor, invoke it from the configuration file
+            self.__invoke_config(config_path=config_path, portal=portal)
+
+    def __invoke_config(self, config_path, portal):
+
+        if not config_path:
+            # Fallback to current working directory
+            fallback = os.path.join(os.getcwd(), "env.ini")
+
+            if os.path.exists(fallback) and os.path.getsize(fallback) > 0:
+                config_path = fallback
+            else:
+                # Fallback to the file root directory
+                fallback = os.path.join(os.path.dirname(__file__), "env.ini")
+
+                if os.path.exists(fallback) and os.path.getsize(fallback) > 0:
+                    config_path = fallback
+
+        if not config_path:
+            raise FileNotFoundError("unable to locate config file")
+
+        config = cfg.Config(filepath=config_path)
+        config.parse_config()
+
+        self.api_key = config.get_key()
+
+        if portal == "subuser":
+            try:
+                self.user_id = config.get_subuser_id()
+            except NoOptionError:
+                self.user_id = None
+
+    @retry(reraise=True,
+           stop=(stop_after_delay(MAX_DELAY) | stop_after_attempt(MAX_RETRIES)),
+           wait=wait_fixed(MAX_WAIT))
     def get(self, url, **kwargs):
-        """ Fulfill Get Request """
+        """Fulfills a HTTP GET Request."""
         data = {key: kwargs[key] for key in kwargs}
         url = self.base_url + url
 
         if self.portal == "main":
-            headers = self.get_headers()
+            headers = self._get_headers()
         if self.portal == "subuser":
-            headers = self.get_subuser_headers()
+            headers = self._get_subuser_headers()
         try:
             r = requests.get(url, params=data, headers=headers)
             r.raise_for_status()
-        except requests.exceptions.ConnectionError as e:
-            print("Connection Error: {!r}".format(e))
-        except requests.exceptions.Timeout as e:
-            print("Request Timeout: {!r}".format(e))
         except requests.exceptions.HTTPError as e:
-            print("Credentials Error: {!r}".format(e))
+            raise requests.exceptions.HTTPError("Authentication Credentials Error: {!r}".format(e))
         else:
             return r
 
-    @retry(stop=(stop_after_delay(MAX_DELAY) | stop_after_attempt(MAX_RETRIES)), wait=wait_fixed(MAX_WAIT))
+    @retry(reraise=True,
+           stop=(stop_after_delay(MAX_DELAY) | stop_after_attempt(MAX_RETRIES)),
+           wait=wait_fixed(MAX_WAIT))
     def post(self, url, **kwargs):
-        """ Fulfill Post Request """
+        """Fulfills a HTTP POST Request."""
         data = {key: kwargs[key] for key in kwargs}
         url = self.base_url + url
 
         if self.portal == "main":
-            headers = self.get_headers()
+            headers = self._get_headers()
         if self.portal == "subuser":
-            headers = self.get_subuser_headers()
+            headers = self._get_subuser_headers()
         try:
             r = requests.post(url, data=data, headers=headers)
             r.raise_for_status()
-        except requests.exceptions.ConnectionError as e:
-            print("Connection Error: {!r}".format(e))
-        except requests.exceptions.Timeout as e:
-            print("Request Timeout: {!r}".format(e))
         except requests.exceptions.HTTPError as e:
-            print("Credentials Error: {!r}".format(e))
+            raise requests.exceptions.HTTPError("Authentication Credentials Error: {!r}".format(e))
         else:
             return r
 
-    @retry(stop=(stop_after_delay(MAX_DELAY) | stop_after_attempt(MAX_RETRIES)), wait=wait_fixed(MAX_WAIT))
+    @retry(reraise=True,
+           stop=(stop_after_delay(MAX_DELAY) | stop_after_attempt(MAX_RETRIES)),
+           wait=wait_fixed(MAX_WAIT))
     def patch(self, url, **kwargs):
-        """ Fulfill Patch Request """
+        """Fulfills a HTTP PATCH Request."""
         data = {key: kwargs[key] for key in kwargs}
         url = self.base_url + url
 
         if self.portal == "main":
-            headers = self.get_headers()
+            headers = self._get_headers()
         if self.portal == "subuser":
-            headers = self.get_subuser_headers()
+            headers = self._get_subuser_headers()
         try:
-            r = requests.patch(url, data=data, headers=headers)
+            r = requests.patch(url, json=data, headers=headers)
             r.raise_for_status()
-        except requests.exceptions.ConnectionError as e:
-            print("Connection Error: {!r}".format(e))
-        except requests.exceptions.Timeout as e:
-            print("Request Timeout: {!r}".format(e))
         except requests.exceptions.HTTPError as e:
-            print("Credentials Error: {!r}".format(e))
+            raise requests.exceptions.HTTPError("Authentication Credentials Error: {!r}".format(e))
         else:
             return r
 
-    @retry(stop=(stop_after_delay(MAX_DELAY) | stop_after_attempt(MAX_RETRIES)), wait=wait_fixed(MAX_WAIT))
+    @retry(reraise=True,
+           stop=(stop_after_delay(MAX_DELAY) | stop_after_attempt(MAX_RETRIES)),
+           wait=wait_fixed(MAX_WAIT))
     def delete(self, url, **kwargs):
-        """ Fulfill Delete Request """
+        """Fulfills a HTTP DELETE Request."""
         data = {key: kwargs[key] for key in kwargs}
         url = self.base_url + url
 
         if self.portal == "main":
-            headers = self.get_headers()
+            headers = self._get_headers()
         if self.portal == "subuser":
-            headers = self.get_subuser_headers()
+            headers = self._get_subuser_headers()
         try:
             r = requests.delete(url, params=data, headers=headers)
             r.raise_for_status()
-        except requests.exceptions.ConnectionError as e:
-            print("Connection Error: {!r}".format(e))
-        except requests.exceptions.Timeout as e:
-            print("Request Timeout: {!r}".format(e))
         except requests.exceptions.HTTPError as e:
-            print("Credentials Error: {!r}".format(e))
+            raise requests.exceptions.HTTPError("Authentication Credentials Error: {!r}".format(e))
         else:
             return r
 
-    def get_headers(self):
-        """ Authorization Headers """
-        headers = {"Authorization": "Token {}".format(self._api_key)}
+    def _get_headers(self):
+        """Main Portal Headers"""
+        headers = {"Authorization": "Token {}".format(self.api_key)}
         return headers
 
-    def get_subuser_headers(self):
-        """ Masquerade as Sub-User """
-        headers = {"X-Webshare-SubUser": "{}".format(self.id),
-                   "Authorization": "Token {}".format(self._api_key)}
+    def _get_subuser_headers(self):
+        """Sub-User Portal Headers"""
+        headers = {"X-Webshare-SubUser": "{}".format(self.user_id),
+                   "Authorization": "Token {}".format(self.api_key)}
 
         return headers
-
-
--- pywebshare/webshare.py
+++ pywebshare/webshare.py
@@ -18,150 +18,250 @@ __endpoints__ = ['/profile/',
 
 
 class Webshare(API):
-    """
-    Simple Python Wrapper for Webshare Proxy API
+    """Simple Python Wrapper for Webshare Proxy's API <https://www.webshare.io/>.
 
-     Implements the API general class and can be invoked with an API Key retrieved
-     from 'https://proxy.webshare.io/userapi/keys'
+     Implements the API() general class and can be invoked with an API Key retrieved
+     from <https://proxy.webshare.io/userapi/keys>.
 
-     If no explicit key is provided, by default, the object reads from the env.ini file
-     in the current working directory.
+     A direct representation of the api_key can be passed to the constructor,
+     else, instantiate the object through a configuration file. Currently Supports .ini file format.
+     By default, the object reads from the env.ini file in the current working directory.
+     A different absolute filepath to the configuration file can be specified with <config_path> parameter.
 
-     To provide access to the subuser portal, provide the optional 'portal' and 'id' parameters
+     To provide access to the subuser portal, provide the optional <portal> and <id> parameters.
      >> client = Webshare(API_KEY, portal="subuser", id=1234)
+
+     Requests can be made to the Proxy Configuration, Proxy List and Proxy Stats APIs as a subuser
 
      Otherwise, portal defaults to 'main'
 
      """
 
     def get_profile(self):
-        """
-        Retrieves your Webshare User Profile Object
-        :return: profile json object
+        """ Returns your Webshare User Profile Object.
+
+        Returns:
+            r.json(): profile json object.
         """
         r = super().get(url='/profile/')
         return r.json()
 
     def get_subscription(self):
-        """
+        """ Returns your Webshare User Subscription Object.
 
-        :return:
+        Returns:
+            r.json(): subscription json object.
         """
         r = super().get(url='/subscription/')
         return r.json()
 
     def get_proxy_config(self):
-        """ Proxy Config """
+        """ Returns your Webshare User Proxy Configuration Object
+
+        Returns:
+            r.json(): subscription json object.
+        """
         r = super().get(url='/proxy/config/')
         return r.json()
 
-    def get_proxy_list(self, page, **kwargs):
-        """ Get Proxy List
-        :param page: Current Proxy Page. Defaults to 1
-        :param kwargs: Optional Argument: countries
+    def get_proxy_list(self, page=1, **kwargs):
+        """ Returns your Webshare User Proxy List.
+
+        This API is paginated and returns multiple proxy list objects.
+
+        Args:
+            page: Proxy Page (Offsets by 250). Defaults to 1.
+
+        Keyword Args:
+            countries (dict of str: int): Dictionary containing country codes in
+                                          in ISO 3166-1 Alpha-2 code format and number
+                                          of proxies.
         """
         r = super().get(url='/proxy/list/', page=page, **kwargs)
         return r.json()
 
     def get_proxy_replacement_info(self):
-        """ Get Replacement Info for your subscription """
+        """ Returns your Webshare User Proxy Replacement Object.
+
+        Returns:
+            r.json(): proxy replacement object.
+        """
         r = super().get(url='/proxy/replacement/info/')
         return r.json()
 
     def get_historical_proxy_replacements(self):
-        """ Retrieves all Previous Historical Proxy Replacements """
+        """ Returns a list containing your historical proxy replacement objects.
+
+        Returns:
+            r.json() (list of dicts): List of Historical Proxy Replacements
+        """
         r = super().get(url='/proxy/replacement/')
         return r.json()
 
     def get_proxy_stats(self):
+        """ Returns your Webshare User Proxy Stats Object.
+
+        Returns:
+            r.json(): proxy stats object.
+        """
         r = super().get(url='/proxy/stats/')
         return r.json()
 
-    def get_subuser(self, id):
-        r = super().get(url='/subuser/', ID=id)
+    def get_subuser(self, user_id):
+        """ Returns your a Sub-users proxy configuration object.
+
+        Args:
+            user_id (int):  ID of the Sub-User.
+
+        Returns:
+            r.json(): sub-user's proxy configuration object.
+        """
+        r = super().get(url='/subuser/', ID=user_id)
         return r.json()
 
-    def get_all_subusers(self, page):
-        """
-        :param page:
-        :return:
-        :raises: HTTPError if Page not Found
+    def get_all_subusers(self, page=1):
+        """ Returns all Sub-Users in the System.
+
+        Args:
+            page (int): Page
+
+        Returns:
+            r.json(): Sub-User's object.
         """
         r = super().get(url='/subuser/', page=page)
         return r.json()
 
-    @staticmethod
-    def get_endpoints():
-        return __endpoints__
+    def update_subuser(self, user_id, **kwargs):
+        """ Update Subuser Information in the System.
 
-    def update_subuser(self, id, **kwargs):
-        """ Update Subuser Information
-        :param kwargs:
-        :return:
+        Args:
+            user_id (int): Subuser ID
+            **kwargs: Optional Keyword Arguments.
+
+        Keyword Args:
+            label (str): Unique user identifier
+            proxy_username (str): The username to connect to the Webshare Proxy.
+            proxy_password (str): The password to connect to the Webshare Proxy.
+            proxy_countries (dict of str: int): Dictionary containing country codes in
+                                                in ISO 3166-1 Alpha-2 code format and number
+                                                of proxies.
+            proxy_limit (int): The user proxy limit in GBs. You can set to 0 in order to get
+                               unlimited bandwidth.
+            max_thread_count (int): The maximum number of proxy request concurrency this subuser
+                                    can have.
         """
-        url = "https://proxy.webshare.io/api/subuser/{0}/".format(id)
+        url = "/subuser/{0}/".format(user_id)
         r = super().patch(url=url, **kwargs)
+        print(r.text)
         return r.json()
 
     def update_proxy_config(self, authorized_ips):
-        """ Update Config Ting Heh
-        :param: authorized_ips: Pee's
+        """Updates your Webshare User Proxy Configuration.
+
+        Args:
+            authorized_ips (list(str)): List of IP Authorized IP Addresses
+                                        [IPs authorized to make requests to Webshare Proxy.]
+
+        Returns:
+            r.json(): updated proxy configuration object.
         """
         if isinstance(authorized_ips, list):
             if all(ipaddress.ip_address(address) for address in authorized_ips):
                 r = super().post(url='/proxy/config/', authorized_ips=authorized_ips)
                 return r.json()
 
-    def reset_proxy_password(self, password):
-        """ Resets The Proxy Password """
+    def reset_proxy_password(self):
+        """ Resets your Proxy Password.
+
+        Returns:
+            r.json(): updated proxy object.
+        """
         r = super().post(url='/proxy/config/reset_password/')
         return r.json()
 
     def refresh_proxy_list(self):
-        """ Refreshes Entire List """
+        """ Refresh your entire proxy list. You can only perform this
+        action if you have on_demand_refreshes_available available.
+
+        Returns:
+            r.json(): updated proxy refresh object.
+        """
         r = super().post(url='/proxy/replacement/info/refresh/')
         return r.json()
 
-    def refresh_subuser_proxy_list(self, id):
-        """ Refresh Proxy List of Subuser
-        :param id:
-        :return:
+    def refresh_subuser_proxy_list(self, user_id):
+        """ Refresh the proxy list of a user. You can only perform this
+        action if the user has a custom proxy list.
+
+        Args:
+            user_id (int): target sub-user ID
+
+        Returns:
+            r.json(): updated sub-user proxy refresh object.
         """
-        url = "https://proxy.webshare.io/api/subuser/{0}/refresh/".format(id)
+        url = "/subuser/{0}/refresh/".format(user_id)
         r = super().post(url=url)
         return r.json()
 
     def replace_proxy(self, ip_address):
-        """ Replaces Single Proxy From List
-        :param: ip_address: Pee Dress
+        """ Replace a specific proxy from your active proxy list.
+
+        Args:
+            ip_address (int): The IP address replaced. May be presented in CIDR
+                              format (with / at the end). If CIDR is missing, assume /32.
+
+        Returns:
+            r.json(): proxy replacement object.
         """
         r = super().post(url='/proxy/replacement/', ip_address=ip_address)
         return r.json()
 
     def delete_proxy_replacement(self, ip_address):
-        """ Delete a Previously Replaced Proxy.
-        :return: 204 on Success
+        """ Delete a previously replaced proxy.
+
+        Args:
+            ip_address (int): The IP address replaced. May be presented in CIDR
+                              format (with / at the end). If CIDR is missing, assume /32.
+
+        Returns:
+            r.status_code: 204 on Success
         """
         r = super().post(url='/proxy/replacement/', ip_address=ip_address)
         return r.status_code
 
     def create_subuser(self, label, proxy_limit, **kwargs):
-        """ Creates Subuser Client
-        :param: label: Name
-        :param: proxy_limit:
-        :param: kwargs:
-        Required: If you wish to gain access to this API, please complete the form at https://proxy.webshare.io/subuser/
+        """ Creates a new Sub-user Client
+
+        Args:
+            label (str): Unique user identifier
+            proxy_limit (int): The user proxy limit in GBs. You can set to 0 in order to get
+                               unlimited bandwidth.
+
+        Keyword Arguments:
+            **max_thread_count (int): The maximum number of proxy request concurrency this subuser
+                                    can have.
+            **proxy_countries (dict of str: int): Dictionary containing country codes in
+                                                in ISO 3166-1 Alpha-2 code format and number
+                                                of proxies.
+
         """
         r = super().post(url='/subuser/', label=label, proxy_limit=proxy_limit, **kwargs)
         return r.json()
 
-    def delete_subuser(self, id):
-        """ Delete's a subuser
-        :param id:
-        :return: 204 Status Code on Success
+    def delete_subuser(self, user_id):
+        """ Delete's a Sub-User
+
+        Args:
+            user_id (int): Sub-User ID
+
+        Returns:
+            r.status_code: 204 on Success
         """
-        url = "https://proxy.webshare.io/api/subuser/{0}/".format(id)
+        url = "/subuser/{0}/".format(id)
         r = super().delete(url=url)
         return r.status_code
 
+    @staticmethod
+    def get_endpoints():
+        return __endpoints__
 
